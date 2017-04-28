63f71ba31d61a5fdfe3538dd0b2a09c26e4471b1
Brandon Garibaldi <bmg.oak@gmail.com>
2017-04-28 11:10:59 -0700

    removes requirement of tasks/seed_me_seymour.rake from lib/sms.rb

--- lib/seed_me_seymour.rb
+++ lib/seed_me_seymour.rb
@@ -2,7 +2,7 @@ require "seed_me_seymour/version"
 require 'active_support/inflector'
 require 'rails/engine'
 # Rails.application.load_tasks
-require 'tasks/seed_me_seymour.rake'
+# require 'tasks/seed_me_seymour.rake'
 
 
 module SeedMeSeymour
